b8ae6f4fb6f76c4c747aca476c500ca2535512fa
Eddie Castillo <46185445+eddiecasti280@users.noreply.github.com>
2021-09-22 20:12:35 -0500

    Counter implementation
    Gamecounter changes

--- tictactoe/Robot_platform.gd
+++ tictactoe/Robot_platform.gd
@@ -4,12 +4,15 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var x_pos : float = 650
+var y_pos : float = 390
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(get_position()) # Replace with function body.
-
+	position = Vector2(650, 390)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if get_parent().game_counter % 3 == 0:
+		position = Vector2(x_pos, y_pos + 1)
+	
--- tictactoe/main.gd
+++ tictactoe/main.gd
@@ -5,6 +5,7 @@ extends Node2D
 var grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]] # 2D array with 3 rows and 3 columns instanciated at 0
 var score : int = 0
 var player_name : String = "MonsieurBloo"
+var game_counter : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +13,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	game_counter += 1
 
 func resize():
 	OS.set_window_size(Vector2(800,500))
