87b7ce56c2de5109002b694df60bffad788ac729
Eddie Castillo <46185445+eddiecasti280@users.noreply.github.com>
2021-09-23 00:00:47 -0500

    Implement character bobbing
    Used delta variable to measure the amount of bobbing each character was doing. Focus on making characters move an angle to differentiate them from the platform they're standing on.

--- tictactoe/Player.gd
+++ tictactoe/Player.gd
@@ -4,15 +4,15 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var x_pos : float = 650
-var y_pos : float = 390
+var x_pos : float = 105.216003
+var y_pos : float = 330.977997
 var up_bob : Vector2 = Vector2(0, 5) 
 var down_bob : Vector2 = Vector2(0, -5)
 var wait : int = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(650, 390)
+	position = Vector2(105.216003, 330.977997)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
